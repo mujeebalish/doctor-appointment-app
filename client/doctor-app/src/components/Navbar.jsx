12efe24e42bf0d8873cbc0526c5e9bea1a2bf4de
mujeebalish <mujeebrehman12122@gmail.com>
2025-07-17 07:03:50 +0500

    Updated code with new changes

---
+++ client/doctor-app/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const navbar = () =>{
+  return (
+    <nav>
+      <div className="navbar_logo">
+        <Link>DoctorApp</Link>
+       <Link> <img src="" alt="" className="img-fluid rounded"/></Link>
+      </div>
+      
+    </nav>
+    )
+}
