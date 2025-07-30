500d5fd23172b5dac72fb89e63d93f795c98c640
mujeebalish <mujeebrehman12122@gmail.com>
2025-07-30 07:19:31 +0500

    Updated doctor route, replaced doctors.js with doctors.json

--- server/Routes/doctorRoutes.js
+++ server/Routes/doctorRoutes.js
@@ -1,11 +1,23 @@
+import path from 'path';
+import fs from 'fs';
 import express from 'express';
-import {doctors} from '../data/doctors.js';
-
+//import {doctors} from '../data/doctors.js';
+const filePath = path.join(process.cwd(),'data','doctors.json');
+const readDoctors = ()=>{
+  try{
+    const doctorsData = fs.readFileSync(filePath,'utf-8');
+    return JSON.parse(doctorsData);
+  }
+  catch(err){
+    console.error(`File read error, ${err.message}`)
+  }
+};
+//console.log(`File path: {$filePath}`);
 //Get Route
 const doctorRoutes = express.Router();
 doctorRoutes.get('/', (req, res) =>{
   try{
-    
+    const doctors = readDoctors();
     res.status(200).send({status: 200, message: 'success', data: doctors})
     
     }
@@ -21,14 +33,16 @@ doctorRoutes.get('/', (req, res) =>{
 // Post Route
 doctorRoutes.post('/post', (req, res) =>{
   try{
+    const doctors = readDoctors();
     const body = req.body;
   const {name, specialty, experience, location, image, description} = body;
     if(!name || !specialty || !experience || ! location || !image || !description){
       throw new Error('All fields are required');
     }
     const newDoctor = {...body, id: doctors.length + 1};
+    
     doctors.push(newDoctor);
-    res.status(201).send({status: 201, message: 'success', data: doctors})
+    res.status(201).send({status: 201, message: 'Doctor added successfully', data: doctors})
   }
   catch(error){
     console.log(error.message);
@@ -39,17 +53,18 @@ doctorRoutes.post('/post', (req, res) =>{
 doctorRoutes.get('/:id', (req, res)=>{
   try{
   const doctorId = parseInt(req.params.id);
+  const doctors = readDoctors();
  const match_D_Id = doctors.find((fDoc)=> fDoc.id === doctorId);
   if(!match_D_Id){
-   return res.status(404).send({status: 404, message: "Doctor not found", data: doctors});
+   return res.status(404).send({status: 404, message: "Doctor not found",});
   }
       res.status(200)
-      .send({statu: 200, message: "doctor added", data: doctors})
+      .send({status: 200, data: match_D_Id})
     
   
   }
   catch(error){
-    console.error(error.message)
+    console.error(error.message);
   }
 });
 export default doctorRoutes;
