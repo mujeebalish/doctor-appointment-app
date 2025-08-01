43263fb3ef3d01e4fd91b3af0d97f017b1a54171
mujeebalish <mujeebrehman12122@gmail.com>
2025-08-01 22:28:51 +0500

    Updated doctorRoutes logic and doctors.json data

--- server/Routes/doctorRoutes.js
+++ server/Routes/doctorRoutes.js
@@ -19,18 +19,15 @@ doctorRoutes.get('/', (req, res) =>{
   try{
     const doctors = readDoctors();
     res.status(200).send({status: 200, message: 'success', data: doctors})
-    
     }
-    
-    
-  
   catch(error){
     console.log(error.message);
-  
 }});
-
-
+    
 // Post Route
+const writeDoctors = ((data) =>{
+      fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
+    });
 doctorRoutes.post('/post', (req, res) =>{
   try{
     const doctors = readDoctors();
@@ -39,17 +36,19 @@ doctorRoutes.post('/post', (req, res) =>{
     if(!name || !specialty || !experience || ! location || !image || !description){
       throw new Error('All fields are required');
     }
-    const newDoctor = {...body, id: doctors.length + 1};
-    
+    const newDoctor = {id: doctors.length + 1, ...body};
     doctors.push(newDoctor);
-    res.status(201).send({status: 201, message: 'Doctor added successfully', data: doctors})
+    
+    writeDoctors(doctors)
+   return res.status(201).send({status: 201, message: 'Doctor added successfully', data: newDoctor})
   }
   catch(error){
-    console.log(error.message);
+   return console.log(error.message);
   }
 });
 
 // Get by :id
+
 doctorRoutes.get('/:id', (req, res)=>{
   try{
   const doctorId = parseInt(req.params.id);
@@ -58,13 +57,39 @@ doctorRoutes.get('/:id', (req, res)=>{
   if(!match_D_Id){
    return res.status(404).send({status: 404, message: "Doctor not found",});
   }
-      res.status(200)
-      .send({status: 200, data: match_D_Id})
-    
-  
+     return res.status(200).send({status: 200, data: match_D_Id})
   }
   catch(error){
     console.error(error.message);
+    return res.status(500).send({status: 500, message: 'Internal server error'})
   }
 });
+// Delete Doctor Route by :id
+doctorRoutes.delete('/deleteDoctor/:id', (req, res) =>{
+  try{
+  const doctorId = parseInt(req.params.id);
+  const doctors = readDoctors();
+  const doctorIndex = doctors.findIndex(doc => doc.id === doctorId);
+  if(doctorIndex === -1 ){
+    return res.status(404).send({status: 404, message: 'Doctor not found'})
+  };
+  const deleteDoctor = doctors.splice(doctorIndex, 1)[0];
+  writeDoctors(doctors);
+  res.status(200).send({status: 200, message: 'Doctor deleted successfully', data: deleteDoctor});
+  } 
+  catch(error)  {
+    console.error(error.message);
+    return res.status(500).send({status: 500, message: 'Internal server error'});
+  }
+})
 export default doctorRoutes;
+    
+    
+  
+  
+
+
+    
+
+    
+  
