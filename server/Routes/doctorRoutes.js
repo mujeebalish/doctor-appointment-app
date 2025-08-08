e690b34930453899c9170accd33454c94b9ca6fe
mujeebalish <mujeebrehman12122@gmail.com>
2025-08-08 12:00:00 +0500

    Updated doctor routes and doctors data

--- server/Routes/doctorRoutes.js
+++ server/Routes/doctorRoutes.js
@@ -18,6 +18,7 @@ const doctorRoutes = express.Router();
 doctorRoutes.get('/', (req, res) =>{
   try{
     const doctors = readDoctors();
+    
     res.status(200).send({status: 200, message: 'success', data: doctors})
     }
   catch(error){
@@ -25,6 +26,7 @@ doctorRoutes.get('/', (req, res) =>{
 }});
 
 // Post Route
+
 const writeDoctors = ((data) =>{
       fs.writeFileSync(filePath, JSON.stringify(data, null, 2), 'utf-8');
     });
@@ -34,7 +36,7 @@ doctorRoutes.post('/post', (req, res) =>{
     const body = req.body;
   const {name, specialty, experience, location, image, description} = body;
     if(!name || !specialty || !experience || ! location || !image || !description){
-      throw new Error('All fields are required');
+      return res.status(400).send({status: 400, message: 'All fields are required'});
     }
     const newDoctor = {id: doctors.length + 1, ...body};
     doctors.push(newDoctor);
@@ -43,7 +45,10 @@ doctorRoutes.post('/post', (req, res) =>{
    return res.status(201).send({status: 201, message: 'Doctor added successfully', data: newDoctor})
   }
   catch(error){
-   return console.log(error.message);
+   console.log(error.message);
+   return res.status(500).send({
+      status: 500,
+      message: 'Internal Server Erro'})
   }
 });
 
@@ -64,7 +69,9 @@ doctorRoutes.get('/:id', (req, res)=>{
     return res.status(500).send({status: 500, message: 'Internal server error'})
   }
 });
+
 // Delete Doctor Route by :id
+
 doctorRoutes.delete('/deleteDoctor/:id', (req, res) =>{
   try{
   const doctorId = parseInt(req.params.id);
@@ -101,6 +108,80 @@ catch(err){
 
 }
 });
+
+// Put
+
+doctorRoutes.put('/updateDoctor/:id', (req, res)=>{
+  try {
+    const doctorId = parseInt(req.params.id);
+    const doctors = readDoctors();
+    const doctorIndex = doctors.findIndex(docInd => docInd.id === doctorId);
+    if(doctorIndex === -1){
+      return res.status(404).send({status: 404, message: 'Doctor not found'});
+    }
+    const updatedDoctor = {...req.body, id: doctorId};
+    doctors[doctorIndex] = updatedDoctor;
+    writeDoctors(doctors);
+    res.status(200).send({status: 200, message: 'Doctor updated successfully ', data: updatedDoctor})
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send({status: 500, message: 'Internal server error'});
+  }
+});
+
+// Patch
+
+doctorRoutes.patch('/patchDoctor/:id', (req, res) => {
+  try{
+    const doctorId = Number(req.params.id);
+    const body = req.body;
+    console.log(body)
+    const doctors = readDoctors();
+    const doctorIndex = doctors.findIndex(doc => doc.id === doctorId);
+    if(doctorIndex === -1) {
+     return res.status(404).send({status: 404, message: 'Doctor not found'});
+    }
+    const existingDoctor = doctors[doctorIndex];
+    const updatedFields = {...existingDoctor, ...body, id: doctorId};
+    doctors[doctorIndex] = updatedFields;
+    writeDoctors(doctors);
+    //console.log(doctors);
+    res.status(200).send({status: 200, message: 'Doctor updated successfully ', data: doctors[doctorIndex]})
+  }
+  catch(err){
+    console.log(err.message);
+    return res.status(500).send({status: 500, message: 'Internal server error '});
+  }
+});
+// Validate Ids
+
+/*doctorRoutes.get('/validateIds', (req, res) => {
+  try {
+    const doctors = readDoctors();
+
+    const existingIds = doctors.map(doc => doc.id).sort((a, b) => a - b);
+
+    const missingIds = [];
+    const maxId = Math.max(...existingIds);
+
+    for (let i = 1; i <= maxId; i++) {
+      if (!existingIds.includes(i)) {
+        missingIds.push(i);
+      }
+    }
+
+    res.status(200).send({
+      status: 200,
+      existingIds,
+      missingIds
+    });
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send({ status: 500, message: 'Internal server error' });
+  }
+}); */
+
 export default doctorRoutes;
 
 
