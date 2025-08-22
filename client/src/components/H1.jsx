4001a4b16b51663f473c4ac83560170e29f903cb
mujeebalish <mujeebrehman12122@gmail.com>
2025-08-21 20:10:06 -0700

    updated

--- client/src/components/H1.jsx
+++ client/src/components/H1.jsx
@@ -1,6 +1,6 @@
 const H1 = ({children, className}) => {
   return(
-    <h1 classNmae={className}>{children}</h1>
+    <h1 className={`text-2xl font-bold text-gray-800${className}`}>{children}</h1>
     )
 };
 export default H1;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,26 +6,30 @@ import Button from '../../components/Button';
 import H1 from '../../components/H1';
 const Home = () => {
 const doctorsArray = doctors;
-const [user, setDoctorName] = useState({doctorName:'',
-foundDoctor: null});
+const [user, setUser] = useState('');
+const [userData, setUserData] = useState('');
 //const [dataValue, setDataValue] = useState('');
 //const [doctor, setDoctor] = useState('');
 //const [getDoctor, setGetDoctor] = useState('');
 const onChangeHandler = (e) => {
-  setDoctorName({...user, doctorName: e.target.value})
+  setUser(e.target.value);
+  console.log(e.target.value);
+
 };
 const doctorHandler = () => {
-  doctorMatch = doctorsArray.find((docArray) => docArray.name.toLocaleLowerCase() === user.doctorName.toLocaleLowerCase() )
-  setDoctorName({...user, foundDoctor : doctorMatch || 'Doctor not found'})
+  // doctorMatch = doctorsArray.find((docArray) => docArray.name.toLocaleLowerCase() === user.doctorName.toLocaleLowerCase() )
+  // setDoctorName({...user, foundDoctor : doctorMatch || 'Doctor not found'});
+
 }
 
   return (
     <>
-      <h1>Home Page</h1>
+      <H1>Home page</H1>
      <Navbar/>
-     <Input type='text' vlaue='user.foundDoctor' onChange='onChangeHandler' className='' placeholder='Search Doctor'/>
-     <Button onClick={doctorHandler} className='bg-red-900 text-white p-2 rounded' >Search </Button>
-     {user.foundDoctor ? <h1>{user.foundDoctor}</h1> : `${user.foundDoctor} doesn't exist`}
+     <Input type='text' vlaue={user} onChange={onChangeHandler} placeholder='Search Doctor'/>
+     <Button onClick={doctorHandler} >Search </Button>
+
+     {user.foundDoctor && <H1>{user.foundDoctor}</H1>}
     </>
     )};
 export default Home;
